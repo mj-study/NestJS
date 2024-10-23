26790e3b6c95f7f939262a559e51d387f13f62fe
minjun <hitobi1014@gmail.com>
2024-10-23 17:21:10 +0900

    feat. boards module 추가

---
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+}
