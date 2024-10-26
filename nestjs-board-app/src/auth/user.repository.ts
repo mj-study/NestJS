a613c3fe23aea0c7cb45f616eb131918e200ded6
minjun <hitobi1014@gmail.com>
2024-10-26 14:26:08 +0900

    feat. signup 구현 - 가입시 username unique 처리

--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,10 +1,30 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(credentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = credentialDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (e: any) {
+      if (e.code === '23505') {
+        throw new ConflictException('Existing Username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
