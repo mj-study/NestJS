8648241814e9715cd71ac5bd0a8c06975d2c8053
minjun <hitobi1014@gmail.com>
2024-11-20 12:43:34 +0900

    feat. mongoose save 전 암호화

--- express-passport-app/src/models/users.model.js
+++ express-passport-app/src/models/users.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
   email: {
@@ -16,15 +17,29 @@ const userSchema = mongoose.Schema({
   }
 })
 
-userSchema.methods.comparePassword = function(plainPassword, cb) {
+const saltRounds = 10;
+userSchema.pre('save', function (next) {
+  let user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash
+        next()
+      })
+    })
+  }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
   // bcrypt compare 비교
   // plain password => client, this.password => DB에 있는 비밀번호
-  if (plainPassword === this.password) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-  return cb({error: 'error'})
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  })
 }
 
 const User = mongoose.model('User', userSchema);
